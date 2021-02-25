bf762699dd5c824bbd958946947aba927fa19113
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-02-25 10:49:00 +0000

    Add day 1 exercises

---
+++ day1/band-name-generator/main.py
@@ -0,0 +1,6 @@
+print('Welcome to the band name generator!')
+
+city = input('What city did you grew up in? \n')
+pet = input('What is the name of your favorite pet? \n')
+
+print('Your band name could be: ' + city + ' ' + pet)
