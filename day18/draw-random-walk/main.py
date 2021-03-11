958ea20800a9a2b1dc26a8e8bf24c4b93ba6213a
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-11 18:33:06 +0100

    Add draw random walk project to day 18

---
+++ day18/draw-random-walk/main.py
@@ -0,0 +1,30 @@
+import turtle as t
+import random
+
+t.colormode(255) 
+timmy = t.Turtle()
+timmy.shape("turtle")
+
+timmy.pensize(15)
+timmy.speed(10)
+
+colors = ["red", "green", "yellow", "blue", "cyan", "magenta", "teal"]
+angles = [0, 90, 180, 270]
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    color = (r, g, b)
+    return color
+
+def draw_random_walk():
+    timmy.color(random_color())
+    angle = random.choice(angles)
+    
+    timmy.setheading(angle)
+    timmy.forward(30)
+
+for _ in range(200):
+    draw_random_walk()
