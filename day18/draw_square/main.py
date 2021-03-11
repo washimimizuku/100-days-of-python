0baa458751c2f6f40dd55e5f2ef78e8f1dae0851
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-11 17:52:44 +0100

    Add draw a square project to day  18

---
+++ day18/draw_square/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("green")
+
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
