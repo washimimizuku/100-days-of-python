fadbd11856f575caf81bf2f7dbce28b59b082eb6
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-14 14:05:00 +0100

    Add slice exercise to day 21

---
+++ day21/slice/main.py
@@ -0,0 +1,16 @@
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+piano_tuple = ("do", "ré", "mi", "fá", "sol", "lá", "si")
+
+print(piano_keys[2:5])
+print(piano_keys[2:])
+print(piano_keys[:5])
+print(piano_keys[2:5:2])
+print(piano_keys[::2])
+print(piano_keys[::-1])
+
+print(piano_tuple[2:5])
+print(piano_tuple[2:])
+print(piano_tuple[:5])
+print(piano_tuple[2:5:2])
+print(piano_tuple[::2])
+print(piano_tuple[::-1])
