cc8464d273f0a4ed2443f3bf8680a7970f3da48d
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-20 22:13:52 +0100

    Add weather pandas exercise to day 25

---
+++ day25/weather-pandas/main.py
@@ -0,0 +1,45 @@
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
+
+# Transform CSV data to dictionary
+data_dict = data.to_dict()
+print(data_dict)
+
+# Transform column to list
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+# Calculate valies based on columns
+average = data["temp"].mean()
+print(average)
+
+max = data["temp"].max()
+print(max)
+
+# Get data in columns
+print(data['condition'])
+print(data.condition)
+
+# Get data in rows
+print(data[data.day == "Monday"])
+print(data[data.temp == data["temp"].max()])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+monday_temp = int(monday.temp)
+monday_temp_f = monday_temp * 9 / 5 + 32
+print(monday_temp_f)
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+print(data)
+
+# Save dataframe as CSV
+data.to_csv("new_data.csv")
