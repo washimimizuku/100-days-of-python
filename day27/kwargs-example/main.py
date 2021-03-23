2ab08da551a895f1de1b24bfa1737ed63bbdeb0f
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-23 21:10:31 +0100

    Improve **kwargs example on day 27

--- day27/kwargs-example/main.py
+++ day27/kwargs-example/main.py
@@ -9,3 +9,20 @@ def calculate(n, **kwargs):
 
 total = calculate(2, add=3, multiply=5)
 print(total)
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw["make"] # With dictionary key is mandatory
+        self.model = kw.get("model") # With get it's optional
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="Dacia", model="Duster")
+print(my_car.make)
+print(my_car.model)
+
+my_car2 = Car(make="Nissan", color="cherry")
+print(my_car2.make)
+print(my_car2.model)
+print(my_car2.color)
