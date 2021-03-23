d27a6b7c65dca82559763d18d55ada33407a8605
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-23 18:08:42 +0100

    Add dictionary comprehension examples to day 26

---
+++ day26/dictionary-comprehension/main.py
@@ -0,0 +1,8 @@
+from random import randint
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freedie", "Nuno", "Angela"]
+students_scores = {student: randint(1, 100) for student in names}
+print(students_scores)
+
+passed_students = {student:score for (student, score) in students_scores.items() if score >= 60}
+print(passed_students)
