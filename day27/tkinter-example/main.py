406021bc37e869bd5d98d3989fff96cbfadc7a65
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-23 21:26:50 +0100

    Improve tkinter example on day 27

--- day27/tkinter-example/main.py
+++ day27/tkinter-example/main.py
@@ -1,12 +1,53 @@
 import tkinter
 
+# Create a new window with configurations
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
 my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
-my_label.pack(expand="True")
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="Really New Text")
+
+# Button
+def button_clicked():
+    print("I got clicked")
+    text = input.get()
+    my_label["text"] = text
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+input = tkinter.Entry(width=10)
+input.pack()
+
+# Text
 
 
+
+# Spinbox
+
+
+
+# Scale
+
+
+
+# Checkbox
+
+
+
+# Radiobutton
+
+
+
+# Listbox
+
+
+
+# "While" loop
 window.mainloop()
