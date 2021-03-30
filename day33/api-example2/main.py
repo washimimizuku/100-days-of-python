1318c82c7b46905f7c839f892db63809d023c5dd
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-30 21:54:20 +0200

    Add API example 2 to day 33

---
+++ day33/api-example2/main.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import requests
+
+
+MY_LAT = 47.402210 # Dietikon latitude
+MY_LONG = 8.392733 # Dietikon longitude
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+print(time_now.hour)
