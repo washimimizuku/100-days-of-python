e93d0e105c3f65b3d7ba2f9eab705cb52de442e3
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-03-31 14:38:44 +0200

    Add type hints demo to day 34

---
+++ day34/type-hints-demo/main.py
@@ -0,0 +1,21 @@
+age: int
+name: string
+height: float
+is_human: bool
+
+age = 18
+name = "Nuno"
+height = 1.78
+is_human = True
+
+def police_check(age: int) -> bool:
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+if police_check(19):
+    print("You may pass.")
+else:
+    print("Pay a fine.")
