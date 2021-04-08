5c21584dd4e22dc80afef443f0b8be3257f00785
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-04-08 12:32:09 +0200

    Add tinder auto swipe to day 50

---
+++ day50/tinder-auto-swiping/main.py
@@ -0,0 +1,67 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from time import sleep
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+FACEBOOK_EMAIL = os.environ.get('FACEBOOK_EMAIL')
+FACEBOOK_PASSWORD = os.environ.get('FACEBOOK_PASSWORD')
+TINDER_URL = "http://www.tinder.com"
+CHROME_DRIVER_PATH = "vendors/chromedriver"
+
+
+driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+
+driver.get(TINDER_URL)
+
+sleep(2)
+login_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
+login_button.click()
+
+sleep(2)
+fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login.click()
+
+sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
+
+email = driver.find_element_by_xpath('//*[@id="email"]')
+password = driver.find_element_by_xpath('//*[@id="pass"]')
+
+email.send_keys(FACEBOOK_EMAIL)
+password.send_keys(FACEBOOK_PASSWORD)
+password.send_keys(Keys.ENTER)
+
+driver.switch_to.window(base_window)
+print(driver.title)
+
+sleep(5)
+allow_location_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+allow_location_button.click()
+notifications_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+notifications_button.click()
+cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+cookies.click()
+
+for n in range(100):
+    sleep(1)
+    try:
+        print("called")
+        like_button = driver.find_element_by_xpath(
+            '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+        like_button.click()
+    except ElementClickInterceptedException:
+        try:
+            match_popup = driver.find_element_by_css_selector(".itsAMatch a")
+            match_popup.click()
+        except NoSuchElementException:
+            sleep(2)
+
+driver.quit()
