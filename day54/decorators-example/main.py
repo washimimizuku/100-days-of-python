8feda63712558f2c898c8e3a9077825c91a862f2
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-04-09 20:14:36 +0200

    Add advanced decorators to decorators example on day 54

--- day54/decorators-example/main.py
+++ day54/decorators-example/main.py
@@ -74,3 +74,27 @@ decorated_function = delay_decorator(say_greeting)
 decorated_function()
 
 say_bye() # Call decorated function
+
+# Advanced python decorator functions
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+    return wrapper
+
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
+    
+
+new_user = User("Nuno")
+new_user.is_logged_in = True
+create_blog_post(new_user)
