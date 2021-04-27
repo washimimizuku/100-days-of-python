c8627a99e571e1676b8cb4f7630b9050127aa598
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-04-27 16:51:40 +0100

    Add edit rating to project library on day 63

--- day63/library/main.py
+++ day63/library/main.py
@@ -43,6 +43,21 @@ def add():
     return render_template('add.html.jinja')
 
 
+@app.route("/edit", methods=['GET', 'POST'])
+def edit():
+    if request.method == "POST":
+        #UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+        
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    return render_template("edit_rating.html.jinja", book=book_selected)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
