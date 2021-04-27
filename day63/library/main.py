d5fc1387e5bbbfb4df616b1ab845323a7a30c3d1
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-04-27 17:08:56 +0100

    Add delete book to project library on day 63

--- day63/library/main.py
+++ day63/library/main.py
@@ -46,16 +46,24 @@ def add():
 @app.route("/edit", methods=['GET', 'POST'])
 def edit():
     if request.method == "POST":
-        #UPDATE RECORD
         book_id = request.form["id"]
         book_to_update = Book.query.get(book_id)
         book_to_update.rating = request.form["rating"]
         db.session.commit()
         return redirect(url_for('home'))
-        
+
     book_id = request.args.get('id')
     book_selected = Book.query.get(book_id)
     return render_template("edit_rating.html.jinja", book=book_selected)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
