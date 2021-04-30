20be373af7383defec3327b45297047533e85b59
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-04-30 11:30:43 +0100

    Add movie project to day 64

---
+++ day64/movie-project/main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SOME_REALLY_SECRET_KEY'
+Bootstrap(app)
+
+# Create database
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# Create movie table
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    image_url = db.Column(db.String(250), nullable=True)
+
+
+db.create_all()
+
+
+@app.route("/")
+def home():
+    all_movies = Movie.query.all()
+    return render_template("index.html.jinja", movies=all_movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
