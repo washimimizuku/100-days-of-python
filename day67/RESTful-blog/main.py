306966c11bb1c827c7354271e86c320cd7b07cd2
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-05 11:03:59 +0200

    Add creation of posts

--- day67/RESTful-blog/main.py
+++ day67/RESTful-blog/main.py
@@ -1,3 +1,4 @@
+from datetime import date
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -11,12 +12,10 @@ app.config['SECRET_KEY'] = 'SOMETHING_REALLY_SECRET'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-# CONNECT TO DB
+# Connect to database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-# CONFIGURE TABLE
 
 
 class BlogPost(db.Model):
@@ -35,22 +34,38 @@ class CreatePostForm(FlaskForm):
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
-# RENDER HOME PAGE USING DB
 @app.route('/')
 def get_all_posts():
     posts = BlogPost.query.all()
     return render_template("index.html.jinja", all_posts=posts)
 
 
-# RENDER POST USING DB
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
     requested_post = BlogPost.query.get(post_id)
     return render_template("post.html.jinja", post=requested_post)
+
+
+@app.route("/new_post", methods=["GET", "POST"])
+def add_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=form.author.data,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html.jinja", form=form)
 
 
 @app.route("/about")
