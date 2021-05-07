6d190762527eee316899b54bccf26b59e8a5cb46
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-07 18:20:03 +0200

    Fix bugs

--- day69/blog-with-users/main.py
+++ day69/blog-with-users/main.py
@@ -200,18 +200,19 @@ def add_new_post():
 @admin_only
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
+
     edit_form = CreatePostForm(
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
+        author=current_user,
         body=post.body
     )
+
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
