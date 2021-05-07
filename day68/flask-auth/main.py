106f74886ebeef181a67f7ba6ed071e9acfccc47
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-05-07 14:20:07 +0200

    Add better error messages

--- day68/flask-auth/main.py
+++ day68/flask-auth/main.py
@@ -34,6 +34,11 @@ def home():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        if User.query.filter_by(email=request.form.get('email')).first():
+            # User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
         hashed_password = generate_password_hash(
             request.form.get('password'),
             method="pbkdf2:sha256",
@@ -47,6 +52,7 @@ def register():
         )
         db.session.add(new_user)
         db.session.commit()
+        login_user(new_user)
 
         return redirect(url_for("secrets"))
 
@@ -60,10 +66,18 @@ def login():
         password = request.form.get('password')
 
         user = User.query.filter_by(email=email).first()
-
-        if check_password_hash(user.password, password):
+        # Email doesn't exist
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        # Password incorrect
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        # Email exists and password correct
+        else:
             login_user(user)
-            return redirect(url_for("secrets"))
+            return redirect(url_for('secrets'))
 
     return render_template("login.html.jinja")
 
