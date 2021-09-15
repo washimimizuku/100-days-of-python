818b7262c0cae8fae821538aa2807b92868ba8d4
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-15 16:03:36 +0200

    highest and lowest earning degrees

--- day71/salaries-by-college-major/main.py
+++ day71/salaries-by-college-major/main.py
@@ -7,4 +7,37 @@ LOCATION = os.path.realpath(os.path.join(
 FULL_PATH = os.path.join(LOCATION, 'salaries_by_college_major.csv')
 
 df = pd.read_csv(FULL_PATH)
-print(df.head())
+clean_df = df.dropna()
+clean_df.tail()
+
+print('Question 1: What college major has the highest mid-career salary? How much do graduates with this major earn? (Mid-career is defined as having 10+ years of experience).')
+
+mid_max = clean_df['Mid-Career Median Salary'].max()
+print(f'Mid-Career Median Salary Maximum Value: {mid_max}')
+mid_max_id = clean_df['Mid-Career Median Salary'].idxmax()
+print(f'Mid-Career Median Salary Maximum ID: {mid_max_id}')
+major_with_max_mid_salary = clean_df['Undergraduate Major'][8]
+print(
+    f'College major with highest mid-career salary: {major_with_max_mid_salary}')
+print()
+
+print('Question 2: Which college major has the lowest starting salary and how much do graduates earn after university?')
+
+start_min = clean_df['Starting Median Salary'].min()
+print(f'Starting Median Salary Minimum Value: {start_min}')
+start_min_id = clean_df['Starting Median Salary'].idxmin()
+print(f'Starting Median Salary Maximum ID: {mid_max_id}')
+major_with_min_start_salary = clean_df['Undergraduate Major'].loc[start_min_id]
+print(
+    f'College major with lowest starting salary: {major_with_min_start_salary}')
+print()
+
+print('Question 3: Which college major has the lowest mid-career salary and how much can people expect to earn with this degree?')
+
+mid_min = clean_df['Mid-Career Median Salary'].min()
+print(f'Mid-Career Median Salary Minimum Value: {mid_min}')
+mid_min_id = clean_df['Mid-Career Median Salary'].idxmin()
+print(f'Mid-Career Median Salary Lowest ID: {mid_min_id}')
+major_with_min_mid_salary = clean_df['Undergraduate Major'].loc[mid_min_id]
+print(
+    f'College major with lowest mid-career salary: {major_with_min_mid_salary}')
