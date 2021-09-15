711689fb4e6c455d66530af059f179214cbcd750
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-15 17:04:01 +0200

    degrees with the highest potential

--- day71/salaries-by-college-major/main.py
+++ day71/salaries-by-college-major/main.py
@@ -41,3 +41,18 @@ print(f'Mid-Career Median Salary Lowest ID: {mid_min_id}')
 major_with_min_mid_salary = clean_df['Undergraduate Major'].loc[mid_min_id]
 print(
     f'College major with lowest mid-career salary: {major_with_min_mid_salary}')
+
+spread_col = clean_df['Mid-Career 90th Percentile Salary'] - \
+    clean_df['Mid-Career 10th Percentile Salary']
+clean_df.insert(1, 'Spread', spread_col)
+clean_df.head()
+
+print('Question 4: Find the top 5 degrees with the highest values in the 90th percentile.')
+highest_potential = clean_df.sort_values(
+    'Mid-Career 90th Percentile Salary', ascending=False)
+print(highest_potential[['Undergraduate Major',
+      'Mid-Career 90th Percentile Salary']].head())
+
+print('Question 5: Which majors have the largest difference between high and low earners after graduation?')
+highest_spread = clean_df.sort_values('Spread', ascending=False)
+print(highest_spread[['Undergraduate Major', 'Spread']].head())
