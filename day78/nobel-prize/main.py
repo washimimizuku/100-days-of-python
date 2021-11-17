0b4dcd6088295c7dc988fb8ed9968f7af71530af
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-11-17 11:49:28 +0100

    analyse prize categories and gender

--- day78/nobel-prize/main.py
+++ day78/nobel-prize/main.py
@@ -46,3 +46,62 @@ denomenator = pd.to_numeric(separated_values[1])
 df_data['share_pct'] = numerator / denomenator
 
 print(df_data.info())
+
+# Percentage of Male vs. Female Winners
+biology = df_data.sex.value_counts()
+fig = px.pie(labels=biology.index,
+             values=biology.values,
+             title="Percentage of Male vs. Female Winners",
+             names=biology.index,
+             hole=0.4,)
+
+fig.update_traces(textposition='inside', textfont_size=15, textinfo='percent')
+
+fig.show()
+
+# First 3 Women
+print(df_data[df_data.sex == 'Female'].sort_values('year', ascending=True)[:3])
+
+# Who won more than once
+is_winner = df_data.duplicated(subset=['full_name'], keep=False)
+multiple_winners = df_data[is_winner]
+print(f'There are {multiple_winners.full_name.nunique()}'
+      ' winners who were awarded the prize more than once.')
+
+col_subset = ['year', 'category', 'laureate_type', 'full_name']
+print(multiple_winners[col_subset])
+
+# Number of prizes per category
+print(df_data.category.nunique())
+
+prizes_per_category = df_data.category.value_counts()
+v_bar = px.bar(
+    x=prizes_per_category.index,
+    y=prizes_per_category.values,
+    color=prizes_per_category.values,
+    color_continuous_scale='Aggrnyl',
+    title='Number of Prizes Awarded per Category')
+
+v_bar.update_layout(xaxis_title='Nobel Prize Category',
+                    coloraxis_showscale=False,
+                    yaxis_title='Number of Prizes')
+v_bar.show()
+
+# First Economics Prize
+print(df_data[df_data.category == 'Economics'].sort_values('year')[:3])
+
+# Male and Female winners by category
+cat_men_women = df_data.groupby(['category', 'sex'],
+                                as_index=False).agg({'prize': pd.Series.count})
+cat_men_women.sort_values('prize', ascending=False, inplace=True)
+
+print(cat_men_women)
+
+v_bar_split = px.bar(x=cat_men_women.category,
+                     y=cat_men_women.prize,
+                     color=cat_men_women.sex,
+                     title='Number of Prizes Awarded per Category split by Men and Women')
+
+v_bar_split.update_layout(xaxis_title='Nobel Prize Category',
+                          yaxis_title='Number of Prizes')
+v_bar_split.show()
