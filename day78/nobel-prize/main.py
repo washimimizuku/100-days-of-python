0bd1f4ab788d8d233477e81d682234a7bd7e4fa6
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-11-17 12:26:11 +0100

    Regional breakdown

--- day78/nobel-prize/main.py
+++ day78/nobel-prize/main.py
@@ -294,3 +294,73 @@ l_chart.update_layout(xaxis_title='Year',
                       yaxis_title='Number of Prizes')
 
 l_chart.show()
+
+# Top Research Places
+
+# Organizations
+top20_orgs = df_data.organization_name.value_counts()[:20]
+top20_orgs.sort_values(ascending=True, inplace=True)
+
+org_bar = px.bar(x=top20_orgs.values,
+                 y=top20_orgs.index,
+                 orientation='h',
+                 color=top20_orgs.values,
+                 color_continuous_scale=px.colors.sequential.haline,
+                 title='Top 20 Research Institutions by Number of Prizes')
+
+org_bar.update_layout(xaxis_title='Number of Prizes',
+                      yaxis_title='Institution',
+                      coloraxis_showscale=False)
+org_bar.show()
+
+# Cities
+top20_org_cities = df_data.organization_city.value_counts()[:20]
+top20_org_cities.sort_values(ascending=True, inplace=True)
+
+city_bar2 = px.bar(x=top20_org_cities.values,
+                   y=top20_org_cities.index,
+                   orientation='h',
+                   color=top20_org_cities.values,
+                   color_continuous_scale=px.colors.sequential.Plasma,
+                   title='Which Cities Do the Most Research?')
+
+city_bar2.update_layout(xaxis_title='Number of Prizes',
+                        yaxis_title='City',
+                        coloraxis_showscale=False)
+city_bar2.show()
+
+# Cities Born
+top20_cities = df_data.birth_city.value_counts()[:20]
+top20_cities.sort_values(ascending=True, inplace=True)
+
+city_bar = px.bar(x=top20_cities.values,
+                  y=top20_cities.index,
+                  orientation='h',
+                  color=top20_cities.values,
+                  color_continuous_scale=px.colors.sequential.Plasma,
+                  title='Where were the Nobel Laureates Born?')
+
+city_bar.update_layout(xaxis_title='Number of Prizes',
+                       yaxis_title='City of Birth',
+                       coloraxis_showscale=False)
+city_bar.show()
+
+# Sunbust Chart
+country_city_org = df_data.groupby(by=['organization_country',
+                                       'organization_city',
+                                       'organization_name'], as_index=False).agg({'prize': pd.Series.count})
+
+country_city_org = country_city_org.sort_values('prize', ascending=False)
+
+burst = px.sunburst(country_city_org,
+                    path=['organization_country',
+                          'organization_city', 'organization_name'],
+                    values='prize',
+                    title='Where do Discoveries Take Place?',
+                    )
+
+burst.update_layout(xaxis_title='Number of Prizes',
+                    yaxis_title='City',
+                    coloraxis_showscale=False)
+
+burst.show()
