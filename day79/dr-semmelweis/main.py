bbdcfe16899ffd75ea18eb82bff02ad92e04ddc0
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-11-18 16:22:03 +0100

    Preliminary data exploration

--- day79/dr-semmelweis/main.py
+++ day79/dr-semmelweis/main.py
@@ -31,3 +31,72 @@ print(df_yearly.head())
 
 print(df_monthly.info())
 print(df_yearly.info())
+
+print(f'Any monthly duplicates? {df_monthly.duplicated().values.any()}')
+print(f'Any yearly duplicates? {df_yearly.duplicated().values.any()}')
+
+print(df_monthly.describe())
+print(df_yearly.describe())
+
+prob = df_yearly.deaths.sum() / df_yearly.births.sum() * 100
+print(f'Chances of dying in the 1840s in Vienna: {prob:.3}%')
+
+# Births and Deaths over time
+
+plt.figure(figsize=(14, 8), dpi=200)
+plt.title('Total Number of Monthly Births and Deaths', fontsize=18)
+
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+
+ax1.grid(color='grey', linestyle='--')
+
+ax1.plot(df_monthly.date,
+         df_monthly.births,
+         color='skyblue',
+         linewidth=3)
+
+ax2.plot(df_monthly.date,
+         df_monthly.deaths,
+         color='crimson',
+         linewidth=2,
+         linestyle='--')
+
+plt.show()
+
+# Create locators for ticks on the time axis
+years = mdates.YearLocator()
+months = mdates.MonthLocator()
+years_fmt = mdates.DateFormatter('%Y')
+
+plt.figure(figsize=(14, 8), dpi=200)
+plt.title('Total Number of Monthly Births and Deaths', fontsize=18)
+plt.yticks(fontsize=14)
+plt.xticks(fontsize=14, rotation=45)
+
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+
+ax1.set_ylabel('Births', color='skyblue', fontsize=18)
+ax2.set_ylabel('Deaths', color='crimson', fontsize=18)
+
+# Use Locators
+ax1.set_xlim([df_monthly.date.min(), df_monthly.date.max()])
+ax1.xaxis.set_major_locator(years)
+ax1.xaxis.set_major_formatter(years_fmt)
+ax1.xaxis.set_minor_locator(months)
+
+ax1.grid(color='grey', linestyle='--')
+
+ax1.plot(df_monthly.date,
+         df_monthly.births,
+         color='skyblue',
+         linewidth=3)
+
+ax2.plot(df_monthly.date,
+         df_monthly.deaths,
+         color='crimson',
+         linewidth=2,
+         linestyle='--')
+
+plt.show()
