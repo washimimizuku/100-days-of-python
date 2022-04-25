624154e773851c39d8d6421e0ba1bd141ea3ba6f
Nuno Barreto <nbarreto@gmail.com>
2022-04-25 16:35:32 +0200

    Improve readability

--- day10/days-in-month-calculator/main.py
+++ day10/days-in-month-calculator/main.py
@@ -22,4 +22,4 @@ def days_in_month(year, month):
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
-print(days)
+print(f"Number of days: {days}")
