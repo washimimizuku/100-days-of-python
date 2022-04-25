4737ef6ab4f4c09c6580e392982e7c6c93fd3ab4
Nuno Barreto <nbarreto@gmail.com>
2022-04-25 16:14:21 +0200

    Improve readability

--- day5/highest-score/main.py
+++ day5/highest-score/main.py
@@ -1,4 +1,4 @@
-student_scores = input("Input a list of student scores ").split()
+student_scores = input("Input a list of student scores separated by spaces: ").split()
 
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
