baaeafaf0005b7460967559cc3635dada045034d
Nuno Barreto <nbarreto@gmail.com>
2022-04-25 16:10:46 +0200

    Improve readability

--- day4/whos-paying/main.py
+++ day4/whos-paying/main.py
@@ -1,6 +1,6 @@
 import random
 
-names_string = input("Give me everybody's names, separated by a comma. ")
+names_string = input("Give me everybody's names, separated by a comma: ")
 names = names_string.split(", ")
 
 unlucky = names[random.randint(0, len(names)) - 1]
